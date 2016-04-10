a13cd2ea7f91739127fdb82711f30f1ff459b4dc
Kyle Stengline <kyle.stengline@gmail.com>
2016-04-10 16:48:51 -0400

    implement listing articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   # ensures that there is a title and body when creating articles.
   validates :title, presence: true
   validates :body, presence: true
+
+  default_scope { order(created_at: :desc) }
 end
