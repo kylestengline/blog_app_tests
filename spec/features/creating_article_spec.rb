fdaf1820bf2057adf5ed9a4c8f0e9f51774d4ec1
Kyle Stengline <kyle.stengline@gmail.com>
2016-04-10 16:10:44 -0400

    comments on creating article spec

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
+# feature spec. 
 RSpec.feature "Creating Articles" do
   scenario "A user creates a new article" do
+    # Testing that we can visit the root path
     visit "/"
 
+    # testing we can click a link called "New Article"
     click_link "New Article"
 
+    #testing that there is a space to fill in a title, body and that we're able to click a button called Create Article.
     fill_in "Title", with: "Creating first article"
     fill_in "Body", with: "Lorem Ipsum"
     click_button 'Create Article'
 
+    # expecting the page to have content and after article saves displays the message below. Also, expecting the page to have an articles_path
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
   end
-
+  # Another scenario for creating article and make sure the form is not blank.
   scenario "A user fails to create a new article" do
     visit "/"
 
@@ -23,6 +28,7 @@ RSpec.feature "Creating Articles" do
     fill_in "Body", with: ""
     click_button 'Create Article'
 
+    # expecting pages to display these messages when article is not created. Had to go into the articles_controller to write an if statement and in the views as well as add validations that the article does have content. See validations in the model.
     expect(page).to have_content("Article has not been created")
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Body can't be blank")
