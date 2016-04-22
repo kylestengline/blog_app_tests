84eac116ca4c439915460c77378de9269a467e75
Kyle Stengline <kyle.stengline@gmail.com>
2016-04-22 11:54:38 -0400

    restrict edit, new article and delete options for owners and signed in users

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -2,10 +2,50 @@ require 'rails_helper'
 
 RSpec.feature "Showing an Article" do 
 
+# Must create two users so we can later log_in as one to create a scenario where non-owners of articles will not see edit or delete. 
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    login_as(john)
-    @article = Article.create(title: "The first article", body: "Body of first article", user: john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com", password: "password")
+    @article = Article.create(title: "The first article", body: "Body of first article", user: @john)
+  end
+
+  scenario "A non-signed in user does not see edit or delete links" do
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+
+  end
+
+# logging in as another user or non-owner of article, they will not be able to see edit or delete actions.  Manipulated the show html to get this to pass.
+  scenario "A non-owner signed in cannot see both links" do
+    
+    login_as(@fred)
+
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+
+  end
+
+  scenario "A signed in owner sees both links" do
+    login_as(@john)
+
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
   # basically saying, "Ok, here's the scenario I want for the page" Then you write that code and manipulate the files you need to in order to pass the test.
   scenario "Display individual article" do
