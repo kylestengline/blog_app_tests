51c663394a1cafa38fea91bb3adfc53e784fca53
Kyle Stengline <kyle.stengline@gmail.com>
2017-01-21 22:17:38 -0800

    removes comment from js

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -1,5 +1,5 @@
 "use strict";
-// Not working when you come back from a different page or click the "Blog App" link
+
 $(document).ready(function(){
   $("button").click(function(){
     $(".navbar-collapse").toggleClass("collapse");
